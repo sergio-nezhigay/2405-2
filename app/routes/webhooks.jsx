57a492afcd430c2876213775b4d17f3261d0d0e5
Sergio Nezhigay <abc1971h@gmail.com>
2024-06-17 13:48:40 +0300

    restore webhooks logging

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -1,39 +1,10 @@
 import { authenticate } from "../shopify.server";
 import db from "../db.server";
 import { json } from "@remix-run/node";
-import crypto from "crypto";
-//const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET;
 
 export const action = async ({ request }) => {
-  console.log("🚀 ~ action:");
-
-  // Step 1: Clone the request and extract the raw payload
-  const reqClone = request.clone();
-  const rawPayload = await reqClone.text();
-  console.log("🚀 ~ rawPayload:", rawPayload);
-
-  // Step 2: Extract the HMAC signature from the request headers
-  const hmacHeader = request.headers.get("X-Shopify-Hmac-Sha256");
-
-  // Step 3: Compute the HMAC signature
-  const generatedHash = crypto
-    .createHmac("sha256", process.env.SHOPIFY_API_SECRET)
-    .update(rawPayload, "utf8")
-    .digest("base64");
-  console.log(
-    "🚀 ~ process.env.SHOPIFY_API_SECRET:",
-    process.env.SHOPIFY_API_SECRET,
-  );
-  console.log("🚀 ~ generatedHash:", generatedHash);
-  console.log("🚀 ~ hmacHeader:", hmacHeader);
-  // Step 4: Compare the computed signature with the one provided by Shopify
-  if (generatedHash !== hmacHeader) {
-    console.log("HMAC validation failed");
-    return new Response("Invalid HMAC signature", { status: 401 });
-  }
-
+  console.log("🚀 ~webhooks action:");
   const { topic, shop, session, admin } = await authenticate.webhook(request);
-  console.log("🚀 ~ topic, shop:", topic, shop);
 
   if (!admin) {
     throw new Response();
@@ -47,17 +18,9 @@ export const action = async ({ request }) => {
       }
       break;
     case "PRODUCTS_UPDATE":
-      console.log("Product was updated");
+      console.log(" case PRODUCTS_UPDATE");
       break;
-    case "CUSTOMERS_DATA_REQUEST":
-      console.log("CUSTOMERS_DATA_REQUEST event");
-      break;
-    case "CUSTOMERS_REDACT":
-      console.log("CUSTOMERS_REDACT event");
-      break;
-    case "SHOP_REDACT":
-      console.log("SHOP_REDACT event");
-      break;
+
     default:
       console.log("Unhandled Webhook Topic:", topic);
       break;
--- app/shopify.server.js
+++ app/shopify.server.js
@@ -28,22 +28,9 @@ const shopify = shopifyApp({
       deliveryMethod: DeliveryMethod.Http,
       callbackUrl: "/webhooks",
     },
-
-    CUSTOMERS_REDACT: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/webhooks",
-    },
-    CUSTOMERS_DATA_REQUEST: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/webhooks",
-    },
-    SHOP_REDACT: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/webhooks",
-    },
   },
   hooks: {
-    afterAuth: async ({ session }) => {
+    afterAuth: async ({ session, admin, webhooks }) => {
       shopify.registerWebhooks({ session });
     },
   },
